304dcbd3a0a589a2de660a2084ec45c8b48c74f9
Mihail Bahirev <7074033@gmail.com>
2021-04-07 17:10:46 +0300

    sticky header

---
+++ js/header-scroll.js
@@ -0,0 +1,21 @@
+var lastScroll = 0;
+var scrollUp = 0;
+var scrollDown = 0;
+var headerSticky = document.querySelector('header');
+var headerHeight = headerSticky.offsetHeight;
+window.addEventListener('scroll', function() {
+    var bodyOffset = document.querySelector('body').getBoundingClientRect().top;
+    if(bodyOffset > lastScroll){
+        scrollDown = 0;
+        scrollUp += bodyOffset - lastScroll;
+            headerSticky.classList.remove('header-hidden');
+            headerSticky.classList.add('header-show');
+        lastScroll = bodyOffset;
+    } else {
+        scrollUp = 0;
+        scrollDown +=  lastScroll - bodyOffset;
+            headerSticky.classList.remove('header-show');
+            headerSticky.classList.add('header-hidden');
+        lastScroll = bodyOffset;
+    }
+  });
