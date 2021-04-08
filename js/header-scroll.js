33337765409cef2bf31fc6c248950f833f1162c6
Mihail Bahirev <7074033@gmail.com>
2021-04-08 09:48:21 +0300

    formatted JS

--- js/header-scroll.js
+++ js/header-scroll.js
@@ -3,19 +3,19 @@ var scrollUp = 0;
 var scrollDown = 0;
 var headerSticky = document.querySelector('header');
 var headerHeight = headerSticky.offsetHeight;
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     var bodyOffset = document.querySelector('body').getBoundingClientRect().top;
-    if(bodyOffset > lastScroll){
+    if (bodyOffset > lastScroll) {
         scrollDown = 0;
         scrollUp += bodyOffset - lastScroll;
-            headerSticky.classList.remove('header-hidden');
-            headerSticky.classList.add('header-show');
+        headerSticky.classList.remove('header-hidden');
+        headerSticky.classList.add('header-show');
         lastScroll = bodyOffset;
     } else {
         scrollUp = 0;
-        scrollDown +=  lastScroll - bodyOffset;
-            headerSticky.classList.remove('header-show');
-            headerSticky.classList.add('header-hidden');
+        scrollDown += lastScroll - bodyOffset;
+        headerSticky.classList.remove('header-show');
+        headerSticky.classList.add('header-hidden');
         lastScroll = bodyOffset;
     }
-  });
+});
